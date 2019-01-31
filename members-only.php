9f9e3ecd8e9be59e2ed43bf0376cfc728dea09fb
Zachary Watkins <watkinza@gmail.com>
2019-01-31 09:44:38 -0600

    Address WordPress coding standards issues

--- members-only.php
+++ members-only.php
@@ -1,6 +1,9 @@
 <?php
 /**
  * Template Name: Private: Must Be Logged In
+ *
+ * @package WordPress
+ * @subpackage AgriFlex4
  */
 
 if ( ! is_user_logged_in() ) {
@@ -8,11 +11,20 @@ if ( ! is_user_logged_in() ) {
 	add_action( 'genesis_entry_content', 'agrilife_members_only' );
 }
 
+/**
+ * Output the message to users who are not logged in
+ *
+ * @since 0.1.0
+ * @return void
+ */
 function agrilife_members_only() {
 
-	printf( '<p>You must <a href="%s" title="login">login</a> to view this page</p>',
+	$output = sprintf(
+		'<p>You must <a href="%s" title="login">login</a> to view this page</p>',
 		wp_login_url()
 	);
+
+	wp_kses_post( $output );
 
 }
 
